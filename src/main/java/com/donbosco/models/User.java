99b7bf27c408a25599ae8dad5737cea178fa1b23
jmat732 <jmatisamuel@gmail.com>
2024-09-22 15:51:29 +0200

    firt commit

--- src/main/java/com/donbosco/models/User.java
+++ src/main/java/com/donbosco/models/User.java
@@ -1,11 +1,21 @@
 package com.donbosco.models;
 
-import jakarta.persistence.*;
-
-
-
 import java.util.HashSet;
 import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
@@ -28,6 +38,14 @@ public class User {
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "role")
     private Set<String> roles = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.MERGE )//merge y persist recomendadas para manytomany
+    @JoinTable(
+        name = "user_flight",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "flight_id")
+    )
+    private Set<Flight> flights = new HashSet<>();
 
     // Constructor sin argumentos
     public User() {}
