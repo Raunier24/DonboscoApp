2305ba6a993144b7775a6c802d39ae6ae37db2a7
Raunier24 <peplisoft@gmail.com>
2024-10-01 11:48:37 +0200

    FlightControllerTest done

--- src/test/java/com/donbosco/services/UserServiceImplTest.java
+++ src/test/java/com/donbosco/services/UserServiceImplTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-class UserServiceImplIntegrationTest {
+class UserServiceImplTest {
 
     @Autowired
     private IUserRepository userRepository;
